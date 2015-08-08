ebdd5f2fc33fba3ef3c2af79f11f7dbfc5c272af
Vignesh <rajvicky2893@gmail.com>
2015-08-08 10:56:10 +0530

    A star program in GIT

---
+++ Star.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class starPyramid {
+
+	public static void main(String agrs[]) {
+		int i,j,n;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the number");
+		n=in.nextInt();
+        System.out.println("The Pattern is");
+        i=1;
+        while(i <=n)
+        {
+            for (j = 1; j <= n-i; j++)
+            {
+                    System.out.print("   ");
+            }
+            for (j = 1; j <= 2*i-1; j++)
+                {
+                    System.out.print(" * ");
+                }
+            i++;
+            System.out.println("");
+            }
+            System.out.println();
+in.close();
+	}
+    }
